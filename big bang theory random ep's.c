cb7697a9885c962d25f29a044158b9a3807a2197
Qwerty1407 <84217835+Qwerty1407@users.noreply.github.com>
2021-06-21 18:08:26 +0530

    Add files via upload

---
+++ big bang theory random ep's.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <cs50.h>
+// S01-  17
+// S02-  23
+// S03-  23
+// S04-  24
+// S05-  24
+// S06-  24
+// S07-  24
+// S08-  24
+// S09-  24
+// S10-  24
+// S11-  24
+// S12-  24
+int main(void)
+{
+    //i = season
+    int i = 12;
+    
+    int e = 0;
+    e++;
+    if(e == 25)
+    {
+        e = 0;
+        return e;
+    }
+    //e = episode
+    int a;
+    
+    int y = 256;
+    int f = 1;
+    f++;
+    while(y > f)
+    {
+        printf("Season - %i ep - %i\n", i , e);
+        e++;
+        if(e == 25)
+            {
+                e = 0;
+                return e;
+
+            }
+        if(e == 25)
+        {
+            i++;
+        }
+    }
+
+}
