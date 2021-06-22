e1f005e847ff767b15f73899e2a3b8a5cb58eb69
Qwerty1407 <84217835+Qwerty1407@users.noreply.github.com>
2021-06-22 17:56:36 +0530

    Add files via upload

---
+++ Shopping trouble.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    float i = get_float("Price of goods: ");
+    while(i <= 0)
+    {
+         i = get_float("Price of goods: ");
+    }
+    float j =get_float("You will pay: ");
+    while(i > j)
+    {
+        printf("Sir/Ma'am we dont work on credit\n");
+       j = get_float("You will pay: ");
+    }
+    if (j == i)
+    {
+        printf("Sir/Ma'am we owe no change.Thank you for buying from your store\n");
+        printf("Come back soon\n");
+    }
+    //program is only letting them pay equal or higher
+    else
+    {
+        float l = j - i;
+        float c = l/25;
+        float q = l/10;
+        printf("We will give you quaters %f", c);
+        printf(" or %f dimes\n", q);
+    }
+}
