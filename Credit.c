1573d83c25c8bde106256a90961e4c54c6e6d33a
Qwerty1407 <84217835+Qwerty1407@users.noreply.github.com>
2021-06-25 16:38:26 +0530

    Add files via upload

---
+++ Credit.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void)
+{
+    printf("%ld\n", time(0));
+
+    string prompt = "1: Pay with Visa\n\
+2: Pay with Paypal\n\
+3: Pay with Master Card\n";
+
+    int i = get_int("%s", prompt);
+    while (i != 1 && i != 2 && i != 3)
+    {
+        i = get_int("%s", prompt);
+    }
+
+    srand(time(0));
+    int code = rand();
+    printf("Your random code is %d\n", code);
+
+    int v = 0;
+    while (v != code)
+    {
+        v = get_int("Enter your random code: ");
+    }
+
+    printf("thank you ji\n");
+}
