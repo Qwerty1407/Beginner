6855823724e70b0083ae7897d37318cfbda198aa
Qwerty1407 <84217835+Qwerty1407@users.noreply.github.com>
2021-07-01 17:09:07 +0530

    Add files via upload

---
+++ Annoyance.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    printf("When an person you do'nt want to talk to a person who is trying to get you'r attention\n");
+    
+    for(int x =100;x>1;x--)
+    {
+        printf("Other Guy: Hi!\n");
+    }
+}
