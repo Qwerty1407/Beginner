36003333643e4f4cebbc47d3f89190b80d6b0928
Qwerty1407 <84217835+Qwerty1407@users.noreply.github.com>
2021-07-01 17:12:25 +0530

    Add files via upload

---
+++ Counting Negative to Positive.c
@@ -0,0 +1,30 @@
+//Astraction and scope
+
+#include <stdio.h>
+#include <cs50.h>
+
+
+int get_negative_int(void);
+int main(void)
+{
+    int x = get_negative_int();
+    printf("Negative integers Couting ");
+    printf("%i\n", x);
+    while (x < 0)
+   {
+    printf("%i\n",x);
+    x++;
+
+   }
+}
+
+int get_negative_int(void)
+{
+    int x;
+    do
+   {
+        x = get_int("Negative Integer: ");
+   }
+   while(x > -1);
+   return x;
+}
